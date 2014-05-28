445a30e2c71238bd405ded06e14b889e384cf541
Jorge Rodríguez Sanz Beto-PC <jergo23@gmail.com>
2014-05-28 12:51:00 +0200

    Delete framework

--- crawler/app/vistas/inicio/index.php
+++
@@ -1,6 +0,0 @@
-<h1>Busqueda con Web Crawler</h1>
-    <form name="form_search" method="post" action="<?php echo \core\URL::generar("inicio/result_search"); ?>">
-        <input id='search' name='search' type="text" value='' />
-        <input id='url' name='url' type="text" value='' />
-        <input type='submit' value='send'/>
-    </form>
